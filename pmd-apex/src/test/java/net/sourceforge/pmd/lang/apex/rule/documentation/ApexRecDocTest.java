405e0411bb1c572d425500010c9246bb8ebb3a39
John Rogers <jrogers@tractionondemand.com>
2019-02-01 13:55:40 -0800

    feat: custom documentation rule for rec that does not require comment

---
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/documentation/ApexRecDocTest.java
@@ -0,0 +1,7 @@
+package net.sourceforge.pmd.lang.apex.rule.documentation;
+
+import net.sourceforge.pmd.testframework.PmdRuleTst;
+
+public class ApexRecDocTest extends PmdRuleTst {
+    // no additional unit tests
+}
